16d09550b715898ce87270e5693b19dc3ea7d532
Samuel <samu3delgado@gmail.com>
2025-12-01 13:20:36 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P_Utilizacion_de_Software.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
