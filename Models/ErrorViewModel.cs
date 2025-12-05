7c610c398cd9c4f4c268ed0dd7f39516fe8b962a
haroldfq7 <haroldfq7@gmail.com>
2025-12-04 21:38:29 -0600

    Borrar
    Eroror view borrado;Modelo

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace P_Utilizacion_de_Software.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
